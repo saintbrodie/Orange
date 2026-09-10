dbe685b6d93fdfde0560bfa4dc8e4bb59d7a31b2
saintbrodie <iamlogansmith@gmail.com>
2026-09-09 22:39:04 -0400

    Refresh theme asset versions

--- app/main.py
+++ app/main.py
@@ -50,10 +50,10 @@ app.include_router(workflow_assets.router)
 
 THEME_HEAD = (
     '    <link rel="stylesheet" href="/static/theme.css?v=2">\n'
-    '    <link rel="stylesheet" href="/static/theme-effects-v2.css?v=1">\n'
-    '    <link rel="stylesheet" href="/static/theme-legacy-bridge.css?v=1">\n'
+    '    <link rel="stylesheet" href="/static/theme-effects-v2.css?v=2">\n'
+    '    <link rel="stylesheet" href="/static/theme-legacy-bridge.css?v=2">\n'
     '    <script src="/static/theme-runtime.js?v=2" defer></script>\n'
-    '    <script src="/static/theme-effects.js?v=1" defer></script>\n'
+    '    <script src="/static/theme-effects.js?v=2" defer></script>\n'
 )
 
 
