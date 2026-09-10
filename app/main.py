16204d9ec9827ffc906d6a9bae606a0d8efb859e
saintbrodie <iamlogansmith@gmail.com>
2026-09-09 22:14:28 -0400

    Apply theme tokens to legacy orange utilities

--- app/main.py
+++ app/main.py
@@ -51,6 +51,7 @@ app.include_router(workflow_assets.router)
 THEME_HEAD = (
     '    <link rel="stylesheet" href="/static/theme.css?v=2">\n'
     '    <link rel="stylesheet" href="/static/theme-effects-v2.css?v=1">\n'
+    '    <link rel="stylesheet" href="/static/theme-legacy-bridge.css?v=1">\n'
     '    <script src="/static/theme-runtime.js?v=2" defer></script>\n'
     '    <script src="/static/theme-effects.js?v=1" defer></script>\n'
 )
