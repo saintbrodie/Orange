304f32cb10feffeb16bf7d54cc25e20c8ccc745e
saintbrodie <iamlogansmith@gmail.com>
2026-09-10 20:55:04 -0400

    Bust refined theme assets

--- app/main.py
+++ app/main.py
@@ -51,9 +51,9 @@ app.include_router(workflow_assets.router)
 THEME_HEAD = (
     '    <link rel="stylesheet" href="/static/theme.css?v=2">\n'
     '    <link rel="stylesheet" href="/static/theme-legacy-bridge.css?v=2">\n'
-    '    <link rel="stylesheet" href="/static/theme-effects-v2.css?v=3">\n'
+    '    <link rel="stylesheet" href="/static/theme-effects-v2.css?v=4">\n'
     '    <script src="/static/theme-runtime.js?v=2" defer></script>\n'
-    '    <script src="/static/theme-effects.js?v=3" defer></script>\n'
+    '    <script src="/static/theme-effects.js?v=4" defer></script>\n'
 )
 
 
